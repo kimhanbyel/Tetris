cdba4cc8f789df003516b4ffc5f9b83e129adbff
inzilion <inzilion@gmail.com>
2023-04-25 14:20:28 +0900

    로그인/로그아웃 완료

--- handlers/auth.js
+++ handlers/auth.js
@@ -6,12 +6,25 @@ const getDate = (d) => `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
 const getTime = (d) => `${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}`;
 const getDateTime = (d) => getDate(d) + ' ' + getTime(d);
 
-const signUpProcess = (req, res)=>{
-  pool.query("SELECT id FROM users", (err, rows, field)=>{
+const signInProcess = (req, res)=>{
+  let sql = `SELECT id, nick FROM users WHERE id=? and pw=?`;
+  let values = [req.body.email, req.body.password];
+  pool.query(sql, values, (err, rows, field)=>{
     if (err) throw err;
-    for(row of rows)
-      if(req.body.email === row.id )
-        return res.render('signUp.html', { msg : '이미 존재하는 이메일입니다.'})
+    if (rows.length>0){
+      req.session.user = {id : rows[0].id, nick : rows[0].nick};
+      res.render('index.html', {user : req.session.user});
+    } else {
+      res.render('signIn.html', {msg : "아이디나 비밀번호가 일치하지 않습니다."});
+    }
+  })
+}
+
+const signUpProcess = (req, res)=>{
+  pool.query(`SELECT id FROM users WHERE id='${req.body.email}'`, (err, rows, field)=>{
+    if (err) throw err;
+    if(rows.length > 0)
+      return res.render('signUp.html', { msg : '이미 존재하는 이메일입니다.'})
     
     let sql = "INSERT INTO users (id, nick, pw, joinDate, lastLogin, tier) VALUES (?, ?, ?, ?, ?, ?)";
     let values = [req.body.email, req.body.nick, req.body.password, 
@@ -23,7 +36,13 @@ const signUpProcess = (req, res)=>{
   })
 }
 
+const signOut = (req, res)=>{
+  req.session.destroy();
+  res.render('index.html');
+}
 
 module.exports = {
+  signInProcess,
   signUpProcess,
+  signOut,
 }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,24 +2,13 @@ const authHandler = require('../handlers/auth.js');
 const express = require('express');
 const router = express.Router();
 
-router.get('/signIn', (req, res)=>{
-  res.render('signIn.html');
-});
+router.get('/signIn', (req, res)=>{ res.render('signIn.html');});
+router.post('/signInProcess', authHandler.signInProcess);
 
-router.post('/signInProcess', (req, res)=>{
-  console.log(req.body);
-  
-  // form에서 전송된 사용자 정보를
-  // DB에서 찾아서 
-  // 있으면 로그인 성공
-  // 없으면 
-  res.render('signUp.html');
-})
-
-router.get('/signUp',  (req, res)=>{
-  res.render('signUp.html');
-});
-
+router.get('/signUp', (req, res)=>{res.render('signUp.html');});
 router.post('/signUpProcess', authHandler.signUpProcess);
+
+router.get('/signOut', authHandler.signOut);
+
 
 module.exports = router;
