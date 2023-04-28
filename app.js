1f01fd310a5072dd4fab06bec28390070e3992e3
inzilion <inzilion@gmail.com>
2023-04-28 13:44:44 +0900

    채팅에 별명 추가(JSON사용) 및 로그인 사용자만 채팅 가능 완료

--- app.js
+++ app.js
@@ -24,13 +24,11 @@ app.use('/game', gameRouter);
 
 wss.on("connection", ws =>{
   console.log("연결되었습니다.");
-  
   ws.on("message", data =>{
     console.log(data.toString());
     for(client of wss.clients)
       client.send(data.toString());
   } )
-  
 })
 
 app.listen(3000);
